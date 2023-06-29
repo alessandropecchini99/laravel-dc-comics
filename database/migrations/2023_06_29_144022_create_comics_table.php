c27fd6bdcbca7cf15acbd0e6d7a00e41cc96d5c7
alessandropecchini99 <pecchini99@gmail.com>
2023-06-29 17:28:16 +0200

    add model, migrate and seeder

---
+++ database/migrations/2023_06_29_144022_create_comics_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title', 50);
+            $table->text('description')->nullable();
+            $table->string('thumb', 400);
+            $table->string('price', 7);
+            $table->string('series', 50);
+            $table->date('sale_date')->nullable();
+            $table->string('type', 20);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
---
+++ database/seeders/ComicsTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call([
+            ComicsTableSeeder::class,
+        ]);
+    }
+}
