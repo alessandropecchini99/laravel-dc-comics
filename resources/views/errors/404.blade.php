2224ccbc8ae97d16fabeefd0672c5cb9ca7be6c7
alessandropecchini99 <pecchini99@gmail.com>
2023-07-01 18:12:49 +0200

    change error 404 page style

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.base')
+
+@section('title', 'Error 404 - Page not found')
+
+@section('main')
+
+    <div class="error404">
+        <div class="errorText">
+            <h1> ERROR 404 - PAGE NOT FOUND </h1>
+        </div>
+    </div>
+    
+@endsection
