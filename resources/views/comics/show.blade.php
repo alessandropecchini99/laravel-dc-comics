ff5bbbd8aa1f121926a9bc07b71f1ac100f73686
alessandropecchini99 <pecchini99@gmail.com>
2023-07-03 19:02:05 +0200

    fixing price

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -14,7 +14,7 @@
             <ul class="list-group" style="margin-bottom: 2em;">
                 <li class="list-group-item"> <span>Title:</span> {{ $comic->title }}</li>
                 <li class="list-group-item"><span>Descriction:</span> {!! $comic->description !!}</li>
-                <li class="list-group-item"><span>Price:</span> {{ $comic->price }}</li>
+                <li class="list-group-item"><span>Price:</span> {{ '$' . ($comic->price / 100) }}</li>
                 <li class="list-group-item"><span>Series:</span> {{ $comic->series }}</li>
                 <li class="list-group-item"><span>Sale date:</span> {{ $comic->sale_date }}</li>
                 <li class="list-group-item"><span>Type:</span> {{ $comic->type }}</li>
