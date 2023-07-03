1b7bace540625480af1dc69703c642b9657ae10a
alessandropecchini99 <pecchini99@gmail.com>
2023-07-04 01:42:39 +0200

    add superbonus 2

--- resources/js/modal.js
+++ resources/js/modal.js
@@ -1,6 +1,8 @@
+// Var
 const myModal = document.getElementById("myModal");
 const myInput = document.getElementById("myInput");
 
+// Event
 myModal.addEventListener("shown.bs.modal", function () {
     myInput.focus();
 });
